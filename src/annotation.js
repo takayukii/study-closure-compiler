891b320bf63cda86a5d10bfbd545e049537f7f73
takayukii <takayukii@gmail.com>
2017-04-10 11:51:38 +0900

    Record type check

--- src/annotation.js
+++ src/annotation.js
@@ -27,15 +27,42 @@
 //
 // foo('aaa', 'bbb');
 
-/**
- * @param {{firstName: string, lastName: string}} props
- * @returns {string}
- */
-function func1 (props) {
-  return `${props.firstName} ${props.lastName}`;
-}
+// /**
+//  * @param {{firstName: string, lastName: string}} props
+//  * @returns {string}
+//  */
+// function func1 (props) {
+//   return `${props.firstName} ${props.lastName}`;
+// }
+//
+// func1({
+//   firstName: 'first_name 1',
+//   familyName: 'family_name 1',
+// });
 
-func1({
+// /**
+//  * @type {{firstName: string, lastName: string}}
+//  */
+// var obj1 = {
+//   firstName: 'first_name 1',
+//   familyName: 'family_name 1',
+// };
+
+/**
+ * @typedef {{
+ *  firstName: string,
+ *  lastName: (string|undefined)
+ * }}
+ */
+var People;
+
+/**
+ * @type {People}
+ */
+var obj2 = {
   firstName: 'first_name 1',
   familyName: 'family_name 1',
-});
+};
+
+// https://github.com/google/closure-compiler/issues/126
+// (string|undefined) means optional in typedef
